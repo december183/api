70e01a24e075a3eb734507406f2978c01c451f4f
december <guanyue584@163.com>
2016-10-07 11:28:51 +0800

    update ServiceController editService method

--- Application/Home/Controller/ServiceController.class.php
+++ Application/Home/Controller/ServiceController.class.php
@@ -40,6 +40,32 @@ class ServiceController extends Controller{
 		}
 	}
 	public function editService(){
-		
+		$data=I('param.');
+		if($_FILES['file']){
+			$arr=$this->upload();
+			foreach($arr as $key=>$path){
+				$
+				$imgArr=getimagesize($path);
+				if($imgArr[0] < 800 && $imgArr[1] < 800){
+					$path=str_replace('\\', '/',$path);
+					$data['mainnail'].=strstr($path,__ROOT__.'/Uploads/image/').';';
+				}else{
+					$data['mainnail'].=$this->thumb($path).';';
+				}
+				if($key == 0){
+					$data['thumbpic']=$this->thumb($path,100,100);
+				}
+			}
+			$data['mainpic']=substr($data['mainpic'],0,-1);
+		}
+		if($this->service->create($data)){
+			if($this->service->save()){
+				$this->apiNotice(200,'商品修改成功');
+			}else{
+				$this->apiNotice(400,'商品修改失败');
+			}
+		}else{
+			$this->apiNotice(401,$this->service->getError());
+		}
 	}
 }
